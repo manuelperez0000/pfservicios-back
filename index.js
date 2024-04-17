edafb8dffd38304fa5c2fb5bbd9d63b0510dbc01
Juan Carlos Avila <juanvs23@gmail.com>
2024-04-17 18:23:58 -0400

    subir para corregir

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config({ path: '.env' })
 const express = require('express')
+const CallBD = require('./db/connnection')
+
 const cors = require('cors')
 const PORT = process.env.PORT || 5000;
 const app = express()
@@ -8,14 +10,15 @@ app.use(cors())
 //zona horaria
 process.env.TZ = "America/Caracas"
 
-const dbConnect = require('./db/conection')
 const morgan = require('morgan')
+
+const callDB = new CallBD()
+callDB.connectToDB();
 
 app.use(morgan('dev'))
 
 app.use(express.json())
-dbConnect()
 
-router(app)
+
 
 app.listen(PORT, () => console.log(`Servidor escuchando en el puerto ${PORT}`))
